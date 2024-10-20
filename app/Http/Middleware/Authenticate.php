8409c8aabee06baf2cbe53e2dc17fd0e37b42ec6
abdelrahman <eng.abdo.com@gmail.com>
2024-10-20 07:44:55 +0300

    add accept:application/json in postman

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Middleware;
 
+use App\Traits\ApiResponse;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
+    use ApiResponse;
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
